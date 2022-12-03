33f0ec7eb8e9d0552f6f8b49652a8308b82f466f
DanuSulaeman <93628272+DanuSulaeman@users.noreply.github.com>
2022-12-03 18:10:53 +0700

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,14 @@
+function chat() {
+    const message = "hai min.. saya mau tanya tentang produk nya"
+    const whatsapp = "https://wa.me/+6281395981980"
+    open(whatsapp, "_blank")
+}
+
+function redirinstagram() {
+    window.location.href = "https://instagram.com/danuslaemn"
+}
+
+function redirmailto() {
+    window.location.href = "mailto:danusulaeman71@gmail.com"
+}
+
